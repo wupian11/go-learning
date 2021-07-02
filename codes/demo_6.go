6ad4e5a143ceaf96edfa49be8a4259ccdff11322
andypianwu <wx_597201f13cf4436eac5de9621d240984@git.code.tencent.com>
2021-07-02 10:11:09 +0800

    demo6

--- codes/demo_6.go
+++ codes/demo_6.go
@@ -2,12 +2,19 @@
 
 package main
 
-func checkAddress(a int) {
+/*
+ * 在Go语言中声明变量时，将在计算机内存中给它分配一个位置，以便能够存储、修改和获取变量的值。
+ * 要获取变量在计算机内存中的地址，可在变量名前加上&字符。
+ */
+func checkValue(a int) {
 	println(a)  //赋值后的值
 	println(&a) //赋值后的内存地址
 }
 
-func checkValue(b *int) {
+/*
+ * 指针是Go语言中的一种类型，指向变量所在的内存单元。要声明指针，可在变量名前加上*字符。
+ */
+func checkPointer(b *int) {
 	println(b)  //赋值后的内存地址
 	println(*b) //赋值后的值
 }
@@ -16,8 +23,8 @@ func main() {
 
 	i := 1
 	println(i)  //i的值
-	println(&i) //i的指针
+	println(&i) //i的内存地址
 
-	checkAddress(i)
-	checkValue(&i)
+	checkValue(i)
+	checkPointer(&i)
 }
